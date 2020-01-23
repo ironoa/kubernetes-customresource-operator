a6ca037d10a83c31e107ba9accc4759c152738b7
Onori Alessio, SUB-BLO (EXT) <Alessio.Onori@swisscom.com>
2020-01-23 12:05:43 +0100

    generated the controller and apis go code with operator-sdk

---
+++ pkg/apis/cache/v1alpha1/customresource_types.go
@@ -0,0 +1,54 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// CustomResourceSpec defines the desired state of CustomResource
+type CustomResourceSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Size of the Deployment
+	Size int32 `json:"size"`
+}
+
+// CustomResourceStatus defines the observed state of CustomResource
+type CustomResourceStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+
+	// Nodes are the names of the CustomResource pods... ?? to check
+	Nodes []string `json:"nodes"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CustomResource is the Schema for the customresources API
+// +kubebuilder:subresource:status
+// +kubebuilder:resource:path=customresources,scope=Namespaced
+type CustomResource struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   CustomResourceSpec   `json:"spec,omitempty"`
+	Status CustomResourceStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// CustomResourceList contains a list of CustomResource
+type CustomResourceList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []CustomResource `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&CustomResource{}, &CustomResourceList{})
+}
